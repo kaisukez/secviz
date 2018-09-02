48aa0e8f5a908ca22d1e4b3ca0de965fa45bb637
= <zykigix@hotmail.com>
2018-09-02 20:35:59 +0700

    add navbar brand and change sidebar color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import StyledNavbar from './styled/Navbar'
+import Brand from './styled/Brand'
 
 export default (props) => {
   return (
     <StyledNavbar>
-      
+      <Brand>secviz portfolio</Brand>
     </StyledNavbar>
   )
 }
---
+++ src/components/styled/Brand.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Brand = styled.h2`
+  text-transform: uppercase;
+  padding-left: 20px;
+  color: white;
+  font-size: 18px;
+  line-height: 60px;
+  margin: 0;
+`
+
+export default Brand
